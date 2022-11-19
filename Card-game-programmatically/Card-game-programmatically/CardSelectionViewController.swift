6bf1390aa27b2a6f40f3cbe9afde82e69711c928
Oleh Dorozhovers <olzer123@gmail.com>
2022-11-19 14:51:07 +0100

    Start configuration, deleted original VC and storyboard. Added firs programmich VC.

---
+++ Card-game-programmatically/Card-game-programmatically/CardSelectionViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+}

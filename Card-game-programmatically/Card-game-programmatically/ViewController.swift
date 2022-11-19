ad461e1db79eb88cdb46b2cd4b4221b6e8eb6b15
Oleh Dorozhovers <olzer123@gmail.com>
2022-11-19 14:29:15 +0100

    Created en empty project

---
+++ Card-game-programmatically/Card-game-programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card-game-programmatically
+//
+//  Created by Oleg Dorozhovets on 19/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

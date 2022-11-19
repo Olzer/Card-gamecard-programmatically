d3dff53241e2c1241b91efe40128e3923cab95d4
Oleh Dorozhovers <olzer123@gmail.com>
2022-11-19 15:04:29 +0100

    Added custom ititializer to buttons

---
+++ Card-game-programmatically/Card-game-programmatically/CardGameButton.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class CardGameButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
